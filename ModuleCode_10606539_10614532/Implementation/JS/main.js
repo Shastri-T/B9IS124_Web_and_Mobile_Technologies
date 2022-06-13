cea78e409b57f0a1c6794a97941e6f3f5f6399b2
shas556 <105118561+shas556@users.noreply.github.com>
2022-06-13 09:11:38 +0100

    Fixed CSS Issues, Deleted unused folders/files
    Fixed CSS Issues, Deleted unused folders/files

--- ModuleCode_10606539_10614532/Implementation/JS/main.js
+++ ModuleCode_10606539_10614532/Implementation/JS/main.js
@@ -1147,6 +1147,9 @@ function GetProductDetailHtml(jsonarray) {
     categorylinkhtml = "";
     final_rating = 0;
     actual_rating = 0;
+    UserRating="";
+    userRatingHtml="";
+
     product_data = products_list.filter(function (i) {
         return i.id == String(getUrlParameter('id'));
     });
@@ -1191,14 +1194,14 @@ function GetProductDetailHtml(jsonarray) {
 
 
     if (product_data[0].features.length == 1) {
-        productimg += "<li class='product_card_desc'> <div style='display:flex;flex-direction:column;'><b><span>" + product_data[0].description + "</span></b><br/>" + ratinghtml + " </span> <br/> <b><span style='color:brown'>" + product_data[0].price + "<div style='float:left'> <div style='display:flex;align-items:center'> <label id='lblqty'>Quantity</label> <select id='productqty'></select> </div> <button id='product_add_to_cart' onclick='AddtoCart()' class='btn card_btn btn-grad'>Add to Cart</button> </div> </span></b></div></li> ";
+        productimg += "<li class='product_card_desc'> <div style='display:flex;flex-direction:column;'><b><span>" + product_data[0].description + "</span></b><br/>" + ratinghtml + " </span> <br/> <b><span style='color:brown'>" + product_data[0].price + "<div style='float:left'> <div style='align-items:center'> <label id='lblqty'>Quantity</label> <select id='productqty'></select> </div> <button id='product_add_to_cart' onclick='AddtoCart()' class='btn card_btn btn-grad'>Add to Cart</button> </div> </span></b></div></li> ";
     }
 
     else {
 
         for (var i = 0; i < product_data[0].features.length; i++) {
             if (i == 0) {
-                productimg += "<li class='product_card_desc'> <div style='display:flex;flex-direction:column;'><b><span>" + product_data[0].description + "</span></b><br/>" + ratinghtml + " </span> <br/> <b><span style='color:brown'>" + product_data[0].price + " <br/> <div style='display:flex;align-items:center'><label id='lblqty'>Quantity &nbsp;</label> <select id='productqty'> </select> </div> <button id='product_add_to_cart' onclick='AddtoCart()' class='btn card_btn btn-grad'>Add to Cart</button> </span></b> <ul style='padding-left:15px'><br/>";
+                productimg += "<li class='product_card_desc'> <div style='display:flex;flex-direction:column;'><b><span>" + product_data[0].description + "</span></b><br/>" + ratinghtml + " </span> <br/> <b><span style='color:brown'>" + product_data[0].price + " <br/> <div style='align-items:center'><label id='lblqty'>Quantity &nbsp;</label> <select id='productqty'> </select> </div> <button id='product_add_to_cart' onclick='AddtoCart()' class='btn card_btn btn-grad'>Add to Cart</button> </span></b> <ul style='padding-left:15px'><br/>";
             }
 
             if (i != 0 && i == product_data[0].features.length - 1) {
@@ -1219,6 +1222,41 @@ function GetProductDetailHtml(jsonarray) {
     for (var i = 0; i < product_data[0].stockcount; i++) {
         $('#productqty').append('<option value="">' + (i + 1) + '</option>');
     }
+
+    $("#modal h2").html("Reviews & Ratings for "+product_data[0].name + "<a id='Modalclosebtn' href='#'>X</a>");
+
+    for (var i = 0; i < product_data[0].reviews.length; i++) {
+    if(product_data[0].reviews[i].rating==5)
+    {
+        UserRating="★★★★★";
+    }
+
+    if(product_data[0].reviews[i].rating==4)
+    {
+        UserRating="★★★★";
+    }
+
+    if(product_data[0].reviews[i].rating==3)
+    {
+        UserRating="★★★";
+    }
+
+    if(product_data[0].reviews[i].rating==2)
+    {
+        UserRating="★★";
+    }
+
+    if(product_data[0].reviews[i].rating==1)
+    {
+        UserRating="★";
+    }
+    
+
+    userRatingHtml +="<br/> <span style='color:orange'>"+UserRating+"</span> by "+product_data[0].reviews[i].person+" : "+product_data[0].reviews[i].comment+"<br/>";
+    
+    }
+
+    $(".copy span").append(userRatingHtml);
 }
 
 /********************** function to generate html for products on landing page ***************************/
